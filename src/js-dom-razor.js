a25bbefd5341eb18823c3aab5e55afd99fa40f35
Ismail <ismail.fareed04@gmail.com>
2019-03-24 15:11:22 +0530

    added basic functions

--- src/js-dom-razor.js
+++ src/js-dom-razor.js
@@ -1,3 +1,41 @@
-getById = funciton(id){
+/***
+getById(id) : returns the element.
+***/
+var getById = function(id){
 	return document.getElementById(id);
+}
+
+/***
+hideById(id) : hides the element.
+***/
+var hideById = function(id){
+	let node = getById(id);
+	if(node){
+		node.style.display='none';
+	}
+}
+
+/***
+showById(id,[optional]displayValue) : shows the element.
+***/
+var showById = function(id,displayValue){
+	let node = getById(id);
+	if(node){
+		node.style.display=displayValue?displayValue:'';
+	}
+}
+
+/***
+textBoxValById(id,[optional]val) : gets the input text box value. Sets the value if given.
+***/
+var textBoxValById = function(id,val){
+	let node = getById(id);
+	if(node){
+		if(val){
+			node.value=val;
+		}
+		else{
+			return node.value;
+		}
+	}
 }
